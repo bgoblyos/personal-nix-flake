e1b59b3ffdad33339cbd30ce1d636f49168fff2b
Bence Göblyös <bence@goblyos.dev>
2026-09-08 21:01:08 +0200

    Add automatic flake checking script

--- home/apps/git.nix
+++ home/apps/git.nix
@@ -3,12 +3,17 @@
 {
   programs.git = {
     enable = true;
+
     settings = {
       user = {
         name  = "Bence Göblyös";
         email = "bence@goblyos.dev";
       };
       init.defaultBranch = "main";
+    };
+
+	extraConfig = {
+      core.hooksPath = ".githooks";
     };
   };
 }
